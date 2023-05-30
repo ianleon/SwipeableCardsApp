9bb13269fe8403a83f41c4110bdfde401d87be04
Ian Leon <iannleon@me.com>
2023-05-30 19:40:51 -0400

    Initial Commit

---
+++ SwipeableCardsApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwipeableCardsApp
+//
+//  Created by Ian Leon on 5/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwipeableCardsApp/SwipeableCardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeableCardsAppApp.swift
+//  SwipeableCardsApp
+//
+//  Created by Ian Leon on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwipeableCardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
